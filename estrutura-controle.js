165920e13494ad853e39d8560577731961916a5c
jeffinho98 <jeffersonfarias98@hotmail.com>
2020-08-10 18:44:21 -0300

    Primeiro commite do código

---
+++ estrutura-controle.js
@@ -0,0 +1,29 @@
+/*escreval("Digite o nome do aluno; ")
+   leia(nome)
+   escreval("Digite a nota 01 do aluno; ")
+   leia(nota1)
+   escreval("Digite a nota 02 do aluno; ")
+   leia(nota2)
+   media := (nota1 + nota2) /2
+   
+   se  media >- 5 entao
+    escreval("aprovado!", nome)
+   senao
+    escreval("reprovado!", nome)
+
+     fimse*/
+
+     var nome, nota01, nota02;
+     nome = prompt("Digite o nome do aluno:")
+     nota1 = prompt("Digite a nota 01 do aluno:")
+     nota02 = prompt("Digite a nota 02 do aluno:")
+
+     media = (parseInt(nota01) + parseInt(nota02)) / 2
+      
+     if (media >= 50){
+         alert("Aprovado!" + nome)
+     }
+
+      else
+          alert("Reprovado!" + nome)     
+     
---
+++ input-output.js
@@ -0,0 +1,8 @@
+
+
+
+var nome, idade;
+nome = prompt("Digite o seu nome:")
+idade = prompt("Digite sua idade")
+alert("O nome digitado foi: " + nome)
+alert("a idade digitada foi: "+ idade)
